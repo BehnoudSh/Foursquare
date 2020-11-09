a8a95926d1cc54e8158c79727d0e645bff8b0163
tinabehnoud <behnoud.sherafati@gmail.com>
2020-11-10 00:52:45 +0330

    adding font and application class

---
+++ app/src/main/java/ir/behnoudsh/aroundme/App.kt
@@ -0,0 +1,25 @@
+package ir.behnoudsh.aroundme
+
+import android.app.Application
+import io.github.inflationx.calligraphy3.CalligraphyConfig
+import io.github.inflationx.calligraphy3.CalligraphyInterceptor
+import io.github.inflationx.viewpump.ViewPump
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        ViewPump.init(
+            ViewPump.builder()
+                .addInterceptor(
+                    CalligraphyInterceptor(
+                        CalligraphyConfig.Builder()
+                            .setDefaultFontPath("fonts/IRANSansMobile_FaNum_.ttf")
+                            .setFontAttrId(R.attr.fontPath)
+                            .build()
+                    )
+                )
+                .build()
+        )
+    }
+}
